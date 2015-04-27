711f4de23e26468ec6761fa22218a05dc534b621
unknown <mithun@mithun-pc.(none)>
2015-04-27 23:05:36 +0600

    Initial Commit for DictionaryApp

---
+++ DictionaryApp/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace DictionaryApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.deatilsTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.showinformationButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(124, 38);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(100, 20);
+            this.idTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Deatils";
+            // 
+            // deatilsTextBox
+            // 
+            this.deatilsTextBox.Location = new System.Drawing.Point(124, 84);
+            this.deatilsTextBox.Name = "deatilsTextBox";
+            this.deatilsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.deatilsTextBox.TabIndex = 3;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(271, 84);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Id";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(109, 163);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // showinformationButton
+            // 
+            this.showinformationButton.Location = new System.Drawing.Point(109, 220);
+            this.showinformationButton.Name = "showinformationButton";
+            this.showinformationButton.Size = new System.Drawing.Size(75, 23);
+            this.showinformationButton.TabIndex = 7;
+            this.showinformationButton.Text = "ShowInformation";
+            this.showinformationButton.UseVisualStyleBackColor = true;
+            this.showinformationButton.Click += new System.EventHandler(this.showinformationButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(250, 163);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 8;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 353);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.showinformationButton);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.deatilsTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox deatilsTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button showinformationButton;
+        private System.Windows.Forms.Button searchButton;
+    }
+}
+
---
+++ DictionaryApp/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DictionaryApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         Dictionary<string , string> dict=new Dictionary<string ,string>();
+
+            private string id;
+            private string details;
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            id = idTextBox.Text;
+            details = deatilsTextBox.Text;
+            dict.Add(id, details);
+            idTextBox.Text = "";
+            deatilsTextBox.Text = "";
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            id = textBox3.Text;
+ 
+     if (dict.ContainsKey(id))
+    {
+    MessageBox.Show("Your Information is in the System");
+    }
+   else
+     {
+       MessageBox.Show("Not Found");
+       showinformationButton.Enabled = false;
+   }
+
+
+
+
+
+
+        }
+
+        private void showinformationButton_Click(object sender, EventArgs e)
+        {
+             foreach (KeyValuePair<string ,string > sPair in dict)
+{
+ MessageBox.Show(sPair.Key+" "+sPair.Value);
+ }
+        }
+    }
+}
